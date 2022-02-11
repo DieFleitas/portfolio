329e844f7e5411c93f4565be2142650307065a6b
DieFleitas <diegofleitas98@outlook.es>
2022-02-11 15:52:12 -0300

    some changes at acomplishments file

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -14,11 +14,12 @@ const Acomplishments = () => (
     <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
-          <BoxNum>{card.number}+</BoxNum>
+          <BoxNum>{`${card.number}+`}</BoxNum>
           <BoxText>{card.text}</BoxText>
         </Box>
       ))}
     </Boxes>
+    <SectionDivider/>
   </Section>
 );
 
