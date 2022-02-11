d6140b92f3dbfe10af17d343d67a4758eb8fad13
DieFleitas <diegofleitas98@outlook.es>
2022-02-11 15:49:16 -0300

    acomplishments section

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -5,23 +5,20 @@ import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
   { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { number: 90, text: 'Github Stars', }
 ];
 
 const Acomplishments = () => (
   <Section>
-    <SectionTitle>Personal Achievements</SectionTitle>
+    <SectionTitle>Personal Acomplishments</SectionTitle>
     <Boxes>
       {data.map((card, index) => (
         <Box key={index}>
-          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxNum>{card.number}+</BoxNum>
           <BoxText>{card.text}</BoxText>
         </Box>
       ))}
     </Boxes>
-    <SectionDivider/>
   </Section>
 );
 
